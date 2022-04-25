a42dfdc071855b865fdc7e51f1dcffefc1d19ae8
shdev2018 <shaunhall.dev@gmail.com>
2022-04-25 18:26:00 +0100

    Dynamic tailwind class config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
+const labelsClasses = [
+  "indigo",
+  "gray",
+  "green",
+  "blue",
+  "red",
+  "purple",
+];
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  //Because we made a dynamic class with the label we need to add those clases
+  // to the safe list so the purge does not remove that
+  safelist: [
+    ...labelsClasses.map((lbl) => `bg-${lbl}-500`),
+    ...labelsClasses.map((lbl) => `bg-${lbl}-200`),
+    ...labelsClasses.map((lbl) => `text-${lbl}-400`)
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
