761511dd279ef3bbaa83d8e839722ef03a03a478
eiksch <eikschmon@hotmail.de>
2022-05-20 21:04:56 +0200

    misc

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { getMonth } from './utils';
 import CalendarHeader from './components/CalendarHeader';
 import Sidebar from './components/Sidebar';
 import Month from './components/Month';
 import EventModal from './components/EventModal';
-import { Dayjs } from 'dayjs';
 import { useAtom } from '@dbeining/react-atom';
 import { MonthIndexAtom } from './atoms/MonthIndexAtom';
 import { updateLabels } from './atoms/SavedEventsAtom';
